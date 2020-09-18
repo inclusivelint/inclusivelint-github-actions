31fde5c576496fde70866582f214a0502b4d1e80
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2020-09-18 12:45:52 -0300

    delete: cleaning repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-echo "HOME"
-ls /github/home
-echo "WORK"
-ls /github/workspace
-echo "FLOW"
-ls /github/workflow
-echo $GITHUB_SHA
-echo "DOCKER CONTAINER"
-ls -la /root
-
 inclusivelint $GITHUB_WORKSPACE
