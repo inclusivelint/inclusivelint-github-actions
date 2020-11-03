ec0d26c728317bbf2ff526cf7409c31a5b96d723
risquass <risquass@microsoft.com>
2020-11-03 11:03:32 -0300

    fixed add and ignore words concatenation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-DIR=~/.inclusivelint
+function define_base_path() {
+    if [[ -z $GITHUB_SHA ]]; then
+        BASE_PATH=~
+    else
+        BASE_PATH=/root
+    fi
+}
+
+define_base_path
+
+DIR=${BASE_PATH}/.inclusivelint
 if [ -d "$DIR" ]; then
     echo "$DIR exists."
 else
@@ -11,13 +21,15 @@ fi
 FILE=$ADDITIONAL_WORDS
 if [ -f "$FILE" ]; then
     echo "$FILE exists. Including additional words to the linter."
-    cat $FILE >> ~/.inclusivelint/outputRelation.txt
+    cat $FILE >> ${BASE_PATH}/.inclusivelint/outputRelation.txt
+    tail ${BASE_PATH}/.inclusivelint/outputRelation.txt
 fi
 
 FILE=$IGNORE_WORDS
 if [ -f "$FILE" ]; then
     echo "$FILE exists. Including list of ignored words."
-    cat $FILE >> ~/.inclusivelint/.inclusivelintignore
+    cat $FILE >> ${BASE_PATH}/.inclusivelint/.inclusivelintignore
+    tail ${BASE_PATH}/.inclusivelint/.inclusivelintignore
 fi
 
 inclusivelint $GITHUB_WORKSPACE
