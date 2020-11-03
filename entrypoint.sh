141add7adc3ca60474b3c20a83869d075d317b65
risquass <risquass@microsoft.com>
2020-11-03 09:30:58 -0300

    add additionalwords and ignoerewords to the pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+DIR=~/.inclusivelint
+if [ -d "$DIR" ]; then
+    echo "$DIR exists."
+else
+    echo "$DIR does not exist. Creating..."
+    mkdir $DIR
+fi
+
+FILE=$ADDITIONAL_WORDS
+if [ -f "$FILE" ]; then
+    echo "$FILE exists. Including additional words to the linter."
+    cat $FILE >> ~/.inclusivelint/outputRelation.txt
+fi
+
+FILE=$IGNORE_WORDS
+if [ -f "$FILE" ]; then
+    echo "$FILE exists. Including list of ignored words."
+    cat $FILE >> ~/.inclusivelint/.inclusivelintignore
+fi
+
 inclusivelint $GITHUB_WORKSPACE
-
-# with:
-#   user_dictionary: /.inclusivelint/.additionalwords
-test -f /inclusivelint/additionalwords && echo /inclusivelint/additionalwords >> ~/.inclusivelint/outputRelation.txt
-
-#   ignore_dictionary: /.inclusivelint/.inclusivelintignore
-test -f /inclusivelint/inclusivelintignore && echo /inclusivelint/inclusivelintignore >> ~/.inclusivelint/.inclusivelintignore
