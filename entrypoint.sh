cd0e7e793c81f64e28aa9c960463faaeca00da09
risquass <risquass@microsoft.com>
2020-11-03 12:17:23 -0300

    additional and ignore words as arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,28 +8,20 @@ function define_base_path() {
     fi
 }
 
+function write_customisations() {
+    if [ -f "$FILE" ]; then
+        cat $FILE >> ${BASE_PATH}/${DEST_FILE}
+    fi
+}
+
 define_base_path
 
-DIR=${BASE_PATH}/.inclusivelint
-if [ -d "$DIR" ]; then
-    echo "$DIR exists."
-else
-    echo "$DIR does not exist. Creating..."
-    mkdir $DIR
-fi
+FILE=$1
+DEST_FILE=.inclusivelint/outputRelation.txt
+write_customisations
 
-FILE=$ADDITIONAL_WORDS
-if [ -f "$FILE" ]; then
-    echo "$FILE exists. Including additional words to the linter."
-    cat $FILE >> ${BASE_PATH}/.inclusivelint/outputRelation.txt
-    tail ${BASE_PATH}/.inclusivelint/outputRelation.txt
-fi
-
-FILE=$IGNORE_WORDS
-if [ -f "$FILE" ]; then
-    echo "$FILE exists. Including list of ignored words."
-    cat $FILE >> ${BASE_PATH}/.inclusivelint/.inclusivelintignore
-    tail ${BASE_PATH}/.inclusivelint/.inclusivelintignore
-fi
+FILE=$2
+DEST_FILE=.inclusivelint/.inclusivelintignore
+write_customisations
 
 inclusivelint $GITHUB_WORKSPACE
