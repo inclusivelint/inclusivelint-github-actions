767263d49fb2c4f883416d5ec4d3b27ecf84d9ed
risquass <risquass@microsoft.com>
2020-11-03 15:01:00 -0300

    raise error if file not found

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,20 +8,31 @@ function define_base_path() {
     fi
 }
 
-function write_customisations() {
+function write_customizations() {
     if [ -f "$FILE" ]; then
         cat $FILE >> ${BASE_PATH}/${DEST_FILE}
+    else 
+        echo "${FILE} not found!"
+        exit 1
     fi
 }
 
 define_base_path
 
-FILE=$1
-DEST_FILE=.inclusivelint/outputRelation.txt
-write_customisations
+if [ -z $1 ]; then
+    echo "No customized words added to inclusivelint!"
+else
+    FILE=$1
+    DEST_FILE=.inclusivelint/outputRelation.txt
+    write_customizations
+fi
 
-FILE=$2
-DEST_FILE=.inclusivelint/.inclusivelintignore
-write_customisations
+if [ -z $2 ]; then
+    echo "No ignored words added to inclusivelint."
+else
+    FILE=$2
+    DEST_FILE=.inclusivelint/.inclusivelintignore
+    write_customizations
+fi
 
 inclusivelint $GITHUB_WORKSPACE
